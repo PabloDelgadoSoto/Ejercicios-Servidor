ad46c56f87b93342d0afbfea85cc5d2fe1668f47
PabloDelgadoSoto <pablodelsot@gmail.com>
2023-11-25 01:51:31 +0100

    diseño agregado

--- Practica 1/index.php
+++ Practica 1/index.php
@@ -5,11 +5,11 @@ include_once "Navegacion.php";
 include_once "Escritor.php";
 
 $request = $_POST;
-
+//uso navegacion para que al iniciar la aplicación por primera vez no aparezca el mensaje de error
 if(Navegacion::esComprar($request)){
     //validacion en el cliente
+    //valido todo incluso producto por si se modifica algo en el select del formulario
     if(Cliente::esCorrecto($request) && Escritor::esCorrecto($request) && Producto::esCorrecto($request)){
-        //valido todo incluso producto por si se modifica algo en el select del formulario
         $cliente = Cliente::crear($request);
         $producto = Producto::obtenerProducto($request);
         $producto->calcularTransporte($cliente);
